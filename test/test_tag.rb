83f1901cf31bfac502b753bbf6bc8a59b0c9c3d5
Robin Stocker <robin@nibor.org>
2010-05-29 23:18:37 +0200

    Add tests and test files

---
+++ test/test_tag.rb
@@ -0,0 +1,21 @@
+require 'helper'
+
+class TestTaglibRuby < Test::Unit::TestCase
+  context "The sample.mp3 file" do
+    setup do
+      fileref = TagLib::FileRef.new("test/data/sample.mp3", false)
+      @tag = fileref.tag
+    end
+
+    should "have basic tag information" do
+      assert_equal 'Dummy Title', @tag.title
+      assert_equal 'Dummy Artist', @tag.artist
+      assert_equal 'Dummy Album', @tag.album
+      assert_equal 'Dummy Comment', @tag.comment
+      assert_equal 'Pop', @tag.genre
+      assert_equal 2000, @tag.year
+      assert_equal 1, @tag.track
+      assert_equal false, @tag.isEmpty
+    end
+  end
+end
