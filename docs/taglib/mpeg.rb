51d869aa337767eaa4730620fa2da1b89abc167b
Robin Stocker <robin@nibor.org>
2011-10-15 19:36:43 +0200

    Add some documentation for TagLib::MPEG::File

---
+++ docs/taglib/mpeg.rb
@@ -0,0 +1,27 @@
+module TagLib::MPEG
+  class File
+    # Load an MPEG file.
+    #
+    # @param [String] filename
+    # @param [Boolean] read_properties if audio properties should be
+    #                  read
+    def initialize(filename, read_properties=true)
+    end
+
+    # Returns a tag that contains attributes from both the ID3v2 and
+    # ID3v1 tag, with ID3v2 attributes having precendence.
+    #
+    # @return [TagLib::Tag]
+    # @return [nil] if not present
+    def tag
+    end
+
+    # Returns the ID3v2 tag.
+    #
+    # @param create if a new tag should be created when none exists
+    # @return [TagLib::ID3v2::Tag]
+    # @return [nil] if not present
+    def id3v2_tag(create=false)
+    end
+  end
+end
