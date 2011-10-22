1a894fced4065f1e3d3ebdbb15d99556c0fd3bdd
Robin Stocker <robin@nibor.org>
2011-10-22 11:28:44 +0200

    Add tests for TagLib::FileRef#audio_properties

---
+++ test/test_fileref_properties.rb
@@ -0,0 +1,21 @@
+require 'helper'
+
+class TestFileRefProperties < Test::Unit::TestCase
+  context "The crash.mp3 file audio properties" do
+    setup do
+      @fileref = TagLib::FileRef.new("test/data/crash.mp3")
+      @properties = @fileref.audio_properties
+    end
+
+    should "exist" do
+      assert_not_nil @properties
+    end
+
+    should "contain basic information" do
+      assert_equal 2, @properties.length
+      assert_equal 157, @properties.bitrate
+      assert_equal 44100, @properties.sample_rate
+      assert_equal 2, @properties.channels
+    end
+  end
+end
