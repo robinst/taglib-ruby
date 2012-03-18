8c4a59cab699170b33cc29a2cf9f98a358919601
Robin Stocker <robin@nibor.org>
2012-03-18 15:48:34 +0100

    Bump version to 0.4.0

--- lib/taglib/version.rb
+++ lib/taglib/version.rb
@@ -1,8 +1,8 @@
 module TagLib
   module Version
     MAJOR = 0
-    MINOR = 3
-    PATCH = 1
+    MINOR = 4
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
