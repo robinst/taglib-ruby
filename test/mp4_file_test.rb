4b0dcf83c22a8dc8290dce7d7ea5c724c7f54906
Jacob Vosmaer <contact@jacobvosmaer.nl>
2012-09-07 12:31:45 +0200

    Test for basic tag information

--- test/mp4_file_test.rb
+++ test/mp4_file_test.rb
@@ -4,12 +4,18 @@ class MP4FileTest < Test::Unit::TestCase
   context "TagLib::MP4::File" do
     setup do
       @file = TagLib::MP4::File.new("test/data/mp4.m4a")
+      @tag = @file.tag
     end
 
-    should "have a tag" do
-      tag = @file.tag
-      assert_not_nil tag
-      assert_equal TagLib::Tag, tag.class
+    should "contain basic tag information" do
+      assert_equal "Title", @tag.title
+      assert_equal "Artist", @tag.artist
+      assert_equal "Album", @tag.album
+      assert_equal "Comment", @tag.comment
+      assert_equal "Pop", @tag.genre
+      assert_equal 2011, @tag.year
+      assert_equal 7, @tag.track
+      assert_equal false, @tag.empty?
     end
 
     teardown do
