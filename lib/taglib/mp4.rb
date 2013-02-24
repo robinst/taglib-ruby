da9423746970b0ffa0fff790c3cc2028e26ec5de
Jacob Vosmaer <contact@jacobvosmaer.nl>
2013-02-25 00:01:19 +0100

    Remove private TagLib classes from Ruby wrapper

--- lib/taglib/mp4.rb
+++ lib/taglib/mp4.rb
@@ -1,13 +1,24 @@
 require 'taglib_mp4'
 
 module TagLib::MP4
+  remove_const :Atom
+  remove_const :AtomData
+  remove_const :Atoms
+  remove_const :IOStream
+
   class File
     extend ::TagLib::FileOpenable
+  end
+
+  class Tag
+    remove_method :save
   end
 
   class ItemListMap
     alias :clear :_clear
     alias :insert :_insert
+    remove_method :_clear
+    remove_method :_insert
 
     def [](key)
       fetch(key)
