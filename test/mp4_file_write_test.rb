1b84b4c8cd102f79f27d4013ec949b6a0863d85f
Jacob Vosmaer <contact@jacobvosmaer.nl>
2013-03-09 15:14:26 +0100

    Add MP4::CoverArt writing test

--- test/mp4_file_write_test.rb
+++ test/mp4_file_write_test.rb
@@ -4,6 +4,7 @@ class MP4FileWriteTest < Test::Unit::TestCase
 
   SAMPLE_FILE = "test/data/mp4.m4a"
   OUTPUT_FILE = "test/data/output.m4a"
+  PICTURE_FILE = "test/data/globe_east_540.jpg"
 
   def reloaded
     TagLib::MP4::File.open(OUTPUT_FILE, false) do |file|
@@ -30,6 +31,28 @@ class MP4FileWriteTest < Test::Unit::TestCase
         file.tag.title
       end
       assert_equal "New Title", written_title
+    end
+
+    should "be able to add and save new cover art" do
+      item_list_map = @file.tag.item_list_map
+      cover_art_list = item_list_map['covr'].to_cover_art_list
+      assert_equal 1, cover_art_list.size
+
+      data = File.open(PICTURE_FILE, 'rb') { |f| f.read }
+      new_cover_art = TagLib::MP4::CoverArt.new(TagLib::MP4::CoverArt::JPEG, data)
+
+      cover_art_list << new_cover_art
+      item_list_map.insert('covr', TagLib::MP4::Item.from_cover_art_list(cover_art_list))
+      assert_equal 2, item_list_map['covr'].to_cover_art_list.size
+
+      success = @file.save
+      assert success
+
+      reloaded do |file|
+        written_cover_art = file.tag.item_list_map['covr'].to_cover_art_list.last
+        assert_equal TagLib::MP4::CoverArt::JPEG, written_cover_art.format
+        assert_equal data, written_cover_art.data
+      end
     end
 
     teardown do
